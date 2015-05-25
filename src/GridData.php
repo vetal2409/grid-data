4803a519cc36af0691b559009ecc47cb5bb6a76f
Vitali Sydorenko <vetal.sydo@gmail.com>
2015-05-25 18:21:56 +0300

    1

---
+++ src/GridData.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Vitali Sydorenko, 2015
+ * @package grid-data
+ * @version 1.0.0
+ */
+
+namespace vetal2409\grid;
+
+class GridData
+{
+    public function init()
+    {
+        echo 123456789;
+    }
+
+    public function __construct() {
+        echo 987654321;
+    }
+}
